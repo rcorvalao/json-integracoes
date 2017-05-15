de7d7e7e69712b95effb06626b5a2c5e01b7911e
eduardoouriques <eduardo.ouriques2@gmail.com>
2017-05-15 11:10:59 -0300

    BKW06 - PriceList

--- json-generate/src/main/java/com/agco/json/generate/entity/MetaData.java
+++ json-generate/src/main/java/com/agco/json/generate/entity/MetaData.java
@@ -4,7 +4,7 @@ import com.github.reinert.jjschema.Attributes;
 
 public class MetaData {
 	
-	@Attributes(maxLength = 40, required = true, description = "Message ID")
+	@Attributes(minLength = 1, maxLength = 40, required = true, description = "Message ID")
 	private String messageId;
 
 	public String getMessageId() {
--- json-generate/src/main/java/com/agco/json/generate/integration/BKW06PriceList.java
+++ json-generate/src/main/java/com/agco/json/generate/integration/BKW06PriceList.java
@@ -1,8 +1,30 @@
 package com.agco.json.generate.integration;
 
+import com.agco.json.generate.entity.MetaData;
+import com.agco.json.generate.entity.PriceList;
 import com.github.reinert.jjschema.Attributes;
 
 @Attributes(title = "BKW06 - PriceList", description = "Send public price list from AGCO to Barkawi")
 public class BKW06PriceList {
+
+	@Attributes(required = true)
+	private PriceList data;
+	private MetaData meta;
+
+	public PriceList getData() {
+		return data;
+	}
+
+	public void setData(PriceList data) {
+		this.data = data;
+	}
+
+	public MetaData getMeta() {
+		return meta;
+	}
+
+	public void setMeta(MetaData meta) {
+		this.meta = meta;
+	}
 
 }
