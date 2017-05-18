7e25c6d07bbc2d94309bfdfbc2282cc60d61639a
Rafael Corvalao <rafael.corvalao@gmail.com>
2017-05-18 12:39:47 -0300

    Teste

--- json-generate/src/main/java/com/agco/json/generate/helper/FileHelper.java
+++ json-generate/src/main/java/com/agco/json/generate/helper/FileHelper.java
@@ -4,8 +4,8 @@ import java.io.File;
 
 public class FileHelper {
 	
-	public final static String JSON_FILES_PATH = "generated-files/jsons/";
-	public final static String SCHEMA_FILES_PATH = "generated-files/schemas/";
+	public final static String JSON_FILES_PATH = "generated-jsons/";
+	public final static String SCHEMA_FILES_PATH = "generated-schemas/";
 
 	public static File createFile(String path, String name, String extension) {
 		return new File(path + name + "." + extension);
